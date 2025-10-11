f148066bcaf352dbae8ecfd4a396be2410ba6e42
Simeon Popov <simeonpopov94@gmail.com>
2025-10-11 18:04:40 +0300

    create the nav bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
 import React, {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image"
 
 const RootLayout = ({children}: {children: ReactNode}) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/logo.svg" width={38} height={32} alt="logo" />
+                    <h2 className="text-primary-100" >PrepWise</h2>
+                </Link>
+            </nav>
+
+            {children}
+        </div>
     )
 }
 export default RootLayout
